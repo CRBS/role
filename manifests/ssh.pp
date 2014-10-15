7a6c72a4cb5e7d77f39a5b3238bf73cbd6a92fc7
Edmond Negado <enegado@rockstar.ucsd.edu>
2014-10-15 15:10:06 -0700

    added firewall rule for ssh

--- manifests/ssh.pp
+++ manifests/ssh.pp
@@ -9,6 +9,14 @@ class role::ssh
     ## the default ssh client.
     ## all common ssh characteristics go here.
     include ssh
+
+    ## open up the default ssh ports in the firewall
+    firewall { '100 allow ssh access':
+        port   => 22,
+        proto  => tcp,
+        action => accept,
+    }
+
 }
 
 
