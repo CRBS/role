e03f31efae1edde6411e12fc5e2b51dec6b7b82d
Edmond Negado <enegado@rockstar.ucsd.edu>
2014-10-15 15:21:06 -0700

    added fw rules for tomcat and apache

--- manifests/webserver.pp
+++ manifests/webserver.pp
@@ -13,12 +13,27 @@ class role::webserver::apache
 {
     include role::webserver
     include apache
+
+    ## open up the proper web ports
+    firewall { '101 allow http and https access':
+        port   => [80, 443],
+        proto  => tcp,
+        action => accept,
+    }
+
 }
 
 class role::webserver::tomcat
 {
     include role::webserver
     include tomcat
+
+    ## open up the proper web ports
+    firewall { '102 allow tomcat 8080 access':
+        port   => 8080,
+        proto  => tcp,
+        action => accept,
+    }
 }
 
 
