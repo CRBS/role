d3817a07a2f1013269e3d3b5cc8f583b1d8618eb
Edmond Negado <enegado@rockstar.ucsd.edu>
2014-10-17 11:16:53 -0700

    adding imagingworkstation role

---
+++ manifests/imagingworkstation.pp
@@ -0,0 +1,13 @@
+###############################################################################
+## role::imagingworkstation
+## This class creates an imaging workstation which includes all the required
+## workstation rpms etc.
+###############################################################################
+class role::imagingworkstation inherits role
+{
+  ## include the puppet_master profile
+  include profile::imaging_workstation
+}
+
+
+
