208c806f682a10530f7b651535410656a2a0dd79
Larry Lui <lawrencellui@gmail.com>
2014-10-21 15:17:36 -0700

    renamed some things

--- manifests/minimalserver.pp
+++ manifests/minimalserver.pp
@@ -3,7 +3,7 @@
 ## This class creates an imaging workstation which includes all the required
 ## workstation rpms etc.
 ###############################################################################
-class role::server::minimal inherits role
+class role::minimalserver inherits role
 {
   ## include the puppet_master profile
   include profile::minimal
